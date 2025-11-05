169345f62ac30f482f2ebc775e9bda547831caa9
Malhar1080 <malhar1080p@gmail.com>
2025-11-05 12:53:23 +0530

    feat(User): Create User entity and model/ Create UserRepository interface

---
+++ backend/monetary/src/main/java/com/assetserve/monetary/model/User.java
@@ -0,0 +1,30 @@
+package com.assetserve.monetary.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable=false)
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+}
---
+++ backend/monetary/src/main/java/com/assetserve/monetary/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.assetserve.monetary.repository;
+
+import com.assetserve.monetary.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
