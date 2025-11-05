3aa231b3acb8e9d470ab4ccd7d1fd525cb00470a
Malhar1080 <malhar1080p@gmail.com>
2025-11-05 10:57:11 +0530

    feat(frontend & backend): inital setup and commit

---
+++ backend/monetary/src/main/java/com/assetserve/monetary/MonetaryApplication.java
@@ -0,0 +1,13 @@
+package com.assetserve.monetary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonetaryApplication.class, args);
+	}
+
+}
